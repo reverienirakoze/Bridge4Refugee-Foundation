913a4b29fd08220487d1ac2e86db6fc350706f67
Reverien Irakoze <reverienirakoze43@gmail.com>
2025-04-13 08:37:32 +0200

    Add Render domain to ALLOWED_HOSTS

--- Bridge4Refugee/settings.py
+++ Bridge4Refugee/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-63vhjq4vv4+1osf=r(v-&+fw^%3*owmn!tn6%60w9^p%vcu5rf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'bridge4refugee-foundation.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -79,10 +81,10 @@ WSGI_APPLICATION = 'Bridge4Refugee.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local development
+        conn_max_age=600
+    )
 }
 
 
@@ -126,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
+    ...
+]
